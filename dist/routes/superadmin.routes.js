fce7bb568596f0b7a7b6c06ef67f947f89a35a6c
exclusive <exclusiveadxe@gmail.com>
2025-11-26 14:25:04 +0100

    fixed alot of issues esp with the dist folder

--- dist/routes/superadmin.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const superadmin_controller_1 = require("../controllers/superadmin.controller");
-const router = (0, express_1.Router)();
-// Intentionally unprotected to bootstrap the first tenant; consider gating behind an env flag in production.
-router.post('/', superadmin_controller_1.createSuperAdmin);
-exports.default = router;
